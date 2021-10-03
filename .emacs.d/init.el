514bf39c9be0c042139c9eaca711b4a626eff6e1
Elam Day-Friedland <elamdf@berkeley.edu>
2021-10-03 01:26:19 -0700

    stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,9 @@
+(call-process "/home/elamd/.local/bin/remaps")
+(call-process "/home/elamd/.scripts/home")
 (set-frame-parameter nil 'fullscreen 'fullboth)
 (setq inhibit-startup-message t)
+(setq doc-view-continuous 1)
+
 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
@@ -14,6 +18,8 @@
 
 (load-theme 'wombat)
 
+
+
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -27,33 +33,43 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(pdf-tools ivy evil sx ##)))
+ '(custom-safe-themes
+   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
+ '(package-selected-packages
+   '(magit request exwm sml-modeline evil-collection pdf-tools ivy evil ##)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(setq evil-want-keybinding nil)
 (require 'evil)
 (evil-mode 1)
+(evil-collection-init)
 
+(require 'smart-mode-line)
+
+(sml/setup)
 
 ; use ivy instead of default
 (ivy-mode)
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
 
-(latex-preview-pane-enable)
+; (latex-preview-pane-enable)
 ; f12 previews, shift f12 creates pdf
 (defun my-latex (action)
   (interactive)
   (if (buffer-modified-p) (save-buffer))
   (let ((f1 (current-frame-configuration))
-        (retcode (shell-command (concat "~/bin/my-latex " action " " buffer-file-name))))
+        (retcode (shell-command (concat "~/.scripts/my-latex " action " " buffer-file-truename))))
     (if (= retcode 0) (set-frame-configuration f1))))
 
-(add-hook 'latex-mode-hook (lambda ()
-      (define-key LaTeX-mode-map (kbd "<f12>") '(lambda () (interactive) (my-latex "preview")))
-      (define-key LaTeX-mode-map (kbd "<S-f12>") '(lambda () (interactive) (my-latex "create")))))
-; for some reason this isn't default
-(add-to-list 'auto-mode-alist '("\\.tex\\'" . latex-mode))
+(add-hook 'LaTeX-mode-hook (lambda ()
+			     (latex-preview-pane-mode)))
+(toggle-debug-on-error)
+(require 'mailcap)
+(add-to-list 'mailcap-user-mime-data
+             '((type . "application/pdf")
+               (viewer . doc-view-mode)))
